d11be8f27b77392d8e04e5c13b9815e9b369160b
Yan Lin <an@MacBook-An.local>
2017-12-05 13:06:32 +0300

    Initial Commit

---
+++ CountryAndState/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CountryAndState
+//
+//  Created by Ян on 05.12.2017.
+//  Copyright © 2017 Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
